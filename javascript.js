ccc51c7c5a08ae14ca798caf1a2fa6312d98bfba
ax-gorgor <80730395+ax-gorgor@users.noreply.github.com>
2023-03-24 19:18:00 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -71,312 +71,276 @@ function onYouTubeIframeAPIReady() {
       
       
        
-     }
+     };
      
 
   if(tiempoDeVideo > 87) {
     a1.textContent = ""
     
   
-  }
+  };
 
 
   if(tiempoDeVideo > 88) {
     
      
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "Hair mer vor herguins ies, surp ieghitsi anun Ko. \n\nPadre nuestro que estás en los cielos, santificado sea Tu nombre; " 
   
 
-  }  
+  };  
 
 
  if(tiempoDeVideo > 93) {
     
      
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "Ieguestsé arkaiutiun Ko ieghitsin gamk Ko vorbes herguinës iev hergrí \n\nvenga a nosotros Tu reino; hágase tu voluntad así en la tierra como en el cielo, " 
   
 
-  } 
+  }; 
  
   if(tiempoDeVideo > 99) {
-      a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+       
     a1.innerText = "zhats mer hanabazort dur mez aisor iev togh mez ëzbardis mer \n\nel pan nuestro de cada día danos hoy y perdonanos nuestras deudas " 
   
-  } 
+  } ;
  
   if(tiempoDeVideo > 107) {
-     a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+    
     a1.innerText = "vorbes iev mek toghumk merots bardabanats \n\nasí como nosotros perdonamos a nuestros deudores" 
   
-  } 
+  }; 
  
   if(tiempoDeVideo > 111) {
-     a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "iev mi danir ëzmez i portsutiún il prguiá ëzmez i charé \n\ny no nos dejes caer en la tentación mas libranos del mal" 
-  } 
+  }; 
  
   if(tiempoDeVideo > 116) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "zi ko e arkaiutiún iev zorutiún iev parrk havidiánës amén \n\nporque es Tuyo el reino y el poder y la gloria, por todos los siglos. Amén" 
-  } 
+  }; 
  
   if(tiempoDeVideo > 124) {
     a1.textContent = ""
     
   
-  }
+  };
  
 
 if(tiempoDeVideo > 128) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+    
     a1.innerText = "Jorhurt jorin anhas anësguizpn \n\nOh misterio profundo, inalcanzable, sin principio," 
-  } 
+  } ;
  
   if(tiempoDeVideo > 135) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "vor zartaretser ëzverin bedutiunt\n\nque has adornado tu supremo" 
-  } 
+  }; 
 
   if(tiempoDeVideo > 142) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "y harrakasd anmaduits lusuin kerabandz\n\nreino en la residencia de la luz inaccesible" 
-  } 
+  }; 
   
  if(tiempoDeVideo > 152) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "  parrok ëztass hreghinats\n\ny con gloria esplendorosa \nlos coros de los ángeles" 
-  }
+  };
 
  
   if(tiempoDeVideo > 160) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "Andjarrahrash zoruteamp sdeghdzer \n\nCon indecible y milagrosa fuerza creaste a Adán," 
-  } 
+  }; 
  
  if(tiempoDeVideo > 167) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+    
     a1.innerText = " ztam badguer diragán,\n\nsemejante a tu señorial imagen," 
-  } 
+  }; 
   
   if(tiempoDeVideo > 172) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+    
     a1.innerText = "iev nazeli parrok zkesdavoretzer,\n\ny vestistele con suntuosa gloria" 
-  } 
+  }; 
  
   if(tiempoDeVideo > 181) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+    
     a1.innerText = "ir trajdn Atení deghí pergranatz.\n\nen el jardín de Edén, lugar de encantos." 
-  } 
+  }; 
 
  
   if(tiempoDeVideo > 192) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "Takavor iergnavor zegueghetsí\n\nRey celestial, mantén inamovible  " 
-  } 
+  } ;
  
   if(tiempoDeVideo > 201.5) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "Ko ansharyh bahiá\n\ntu iglesia y preserva" 
-  } 
+  }; 
  
   
   if(tiempoDeVideo > 208) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "iev zergërbakus anvant kum bahiá\n\na los adoradores de " 
-  } 
+  }; 
 
  
   if(tiempoDeVideo > 218) {
-    a1.style.color = 'black';
-    a1.style.fontFamily = 'Poppins';
-    a1.style.fontSize = '24px';
+     
     a1.innerText = "y jaghaghutián\n\ntu santo nombre en paz" 
-  } 
+  }; 
    
  
   if(tiempoDeVideo > 228.5) {
      a1.textContent = ""
-  } 
+  }; 
 
   
  if(tiempoDeVideo > 229) {
    
     a1.innerText = " iev vasn srpuhvó \n\ny por la intercesión" 
-  } 
+  }; 
 
    if(tiempoDeVideo > 230.5) {
     
     a1.innerText = "Asdvadzadznín parejosuteánn\n\nde la Santísima Madre de Dios" 
-  } 
+  }; 
 
    
 
   if(tiempoDeVideo > 233) {
     
     a1.innerText = "Der ëngal zaghacháns \n\nOh Señor acepta nuestras  " 
-  } 
+  }; 
 
   if(tiempoDeVideo > 234.5) {
    a1.innerText = " mer iev guetsó ëzmez \n\nsúplicas y presérvanos" 
-  } 
+  }; 
  
   if(tiempoDeVideo > 236.5) {
     a1.innerText = " Surp Zasdvadzadzínn iev zamenain \n\nQue la Santa Madre de Dios  " 
-  } 
+  }; 
 
   if(tiempoDeVideo > 238.5) {
     a1.innerText = "surps parejós \n\ny todos los santos intercedan" 
 
-  } 
+  }; 
  
  
   if(tiempoDeVideo > 240) {
     a1.innerText = "arastsuk arr Hair y herguinës \n\nante el Padre en el cielo, para que tenga" 
-  } 
+  }; 
 
 
   if(tiempoDeVideo > 242.5) {
        a1.innerText = "zi gametseal voghormestsí \n\nvoluntad de dar su misericordia" 
 
-  } 
+  }; 
  
   if(tiempoDeVideo > 244) {
      a1.innerText = "iev ktatseal guetsustsé zararadzës Iur  \n\ny preserve con compasión a sus criaturas." 
-  } 
+  }; 
 
   if(tiempoDeVideo > 247.5) {
      a1.innerText = "Amenagal Der Asdvadz mer \n\nOh Dios Todopoderoso" 
 
-  } 
+  } ;
  
   if(tiempoDeVideo > 249) {
     a1.innerText = "guetsó iev voghormeá \n\nSeñor nuestro presérvanos y ten misericordia" 
      
-  } 
+  }; 
  
   if(tiempoDeVideo > 251) {
     a1.innerText = " Der voghormeá, Der voghormeá, Der voghormeá, \n\nSeñor ten misericordia, Señor ten misericordia, Señor ten misericordia, " 
-  } 
+  }; 
  
   if(tiempoDeVideo > 255) {
     a1.innerText = "ëngal Der zaghacháns mer \n\nOh Señor acepta nuestras " 
 
-  } 
+  }; 
 
   if(tiempoDeVideo > 257) {
       a1.innerText = "parejosuteamp surp \n\nsúplicas por intercesión de la" 
 
-  } 
+  }; 
  
   if(tiempoDeVideo > 259) {
    a1.innerText = "Asdvadzadznín anarad dznoghí \n\nSanta Madre de Dios," 
-  } 
+  } ;
   
 
    
   if(tiempoDeVideo > 261.5) {
       a1.innerText = "Miadzní Vortvó ko \n\nMadre inmaculada de tu Unigénito Hijo"
  
-  } 
+  } ;
   
 
   
   if(tiempoDeVideo > 263.5) {
    a1.innerText = "iev aghachanok amenain srpots kots \n\ny mediante el ruego de todos los Santos" 
-  } 
+  }; 
  
 
   
   if(tiempoDeVideo > 268) {
-   a1.innerText = "Lur mez Der iev voghormeá,\n\nOyenos, ¡Oh Señor! Y ten misericordia." } 
+   a1.innerText = "Lur mez Der iev voghormeá,\n\nOyenos, ¡Oh Señor! Y ten misericordia." }; 
   
 
   
   if(tiempoDeVideo > 270.5) {
- a1.innerText = "nereá, kaveá,\n\nPerdona y sé propicio" } 
+ a1.innerText = "nereá, kaveá,\n\nPerdona y sé propicio" }; 
   
 
    
   if(tiempoDeVideo > 271.5) {
- a1.innerText = "iev togh zmeghs mer \n\ny remite nuestros pecados. Haznos" } 
+ a1.innerText = "iev togh zmeghs mer \n\ny remite nuestros pecados. Haznos" }; 
  
 
      
   if(tiempoDeVideo > 274) {
    a1.innerText = "Aryanavoreá kohuteamp parravorel \n\ndignos de glorificarte" 
-  } 
+  }; 
    
 
      
   if(tiempoDeVideo > 276) {
    a1.innerText = "ëzkez ënt Vortvó iev ënt Srpó \n\ncon gratitud con el Hijo y con el Espíritu Santo." 
-  } 
+  }; 
    
 
        
   if(tiempoDeVideo > 278.5) {
    a1.innerText = "Hokvuit aiyëm iev mishd  \n\nAhora y siempre  " 
-  } 
+  } ;
    
 
       
   if(tiempoDeVideo > 280) {
    a1.innerText = "iev havideáns havideníts Amén \n\ny por los siglos de los siglos. Amén." 
-  } 
+  } ;
    
 
      
   if(tiempoDeVideo > 285) {
    a1.innerText = "Meghá Asdudzó\n\nHe pecado ante Dios." 
-  } 
+  }; 
    
 
          
   if(tiempoDeVideo > 287) {
    a1.innerText = "Josdovanim arrachí Asdudzó\n\nConfieso delante de Dios  " 
-  } 
+  }; 
    
 
           
   if(tiempoDeVideo > 289) {
    a1.innerText = "  iev Srpuhvó Asdvadzadznín\n\ny de la Santa Madre de Dios" 
-  } 
+  }; 
    
 
 
@@ -384,19 +348,19 @@ if(tiempoDeVideo > 128) {
          
   if(tiempoDeVideo > 291) {
    a1.innerText = "iev arrachí amenain srpots \n\ny delante de todos los santos " 
-  } 
+  }; 
    
 
            
   if(tiempoDeVideo > 293) {
    a1.innerText = "  iev arrachí tser\n\ny delante vuestro," 
-  } 
+  }; 
    
 
          
   if(tiempoDeVideo > 295) {
    a1.innerText = "hark iev ieghpark zamenain meghs\n\npadres y hermanos, todos mis pecados cometidos." 
-  } 
+  }; 
    
 
   
@@ -404,53 +368,196 @@ if(tiempoDeVideo > 128) {
         
   if(tiempoDeVideo > 298) {
    a1.innerText = "zors kordzeal em Kanzí meghá jorhrtov\n\nPorque he pecado con el pensamiento," 
-  } 
+  }; 
   
 
 
           
   if(tiempoDeVideo > 302) {
    a1.innerText = "paniv iev kordzov iev amenain \n\nla palabra y la acción y" 
-  } 
+  }; 
    
 
             
   if(tiempoDeVideo > 304) {
    a1.innerText = " meghok zor kordzen martig\n\ncon todos los pecados que cometen los hombres." 
-  } 
+  } ;
   
 
 
          
   if(tiempoDeVideo > 305) {
    a1.innerText = "Meghá, meghá. Aghachém ëztsez \n\nHe pecado sí he pecado." 
-  } 
+  }; 
    
          
   if(tiempoDeVideo > 308) {
    a1.innerText = "jntretsék vasn im Hasdudzó ëztoghutiún \n\nLes ruego pidan por mí el perdón de Dios" 
-  } 
+  } ;
   
 
-  /*---------
-  -----------
-  -----------
-  -----------*/
-        /*
-  if(tiempoDeVideo > 259) {
-   a1.innerText = "mer parejosuteamp surp \n\nsúplicas por intercesión de la" 
-  } 
-  */
-        /*
-  if(tiempoDeVideo > 259) {
-   a1.innerText = "mer parejosuteamp surp \n\nsúplicas por intercesión de la" 
-  } 
-  */
-        /*
-  if(tiempoDeVideo > 259) {
-   a1.innerText = "mer parejosuteamp surp \n\nsúplicas por intercesión de la" 
-  } 
-  */
+ 
+  if(tiempoDeVideo > 312) {
+    a1.innerText = " Der voghormeá, Der voghormeá, Der voghormeá, \n\nSeñor ten misericordia, Señor ten misericordia, Señor ten misericordia, " 
+  }; 
+   
+        
+  if(tiempoDeVideo > 316) {
+   a1.innerText = "Voghormestsí kez Asdvadz hzorn \n\nQue el Dios Todopoderoso tenga misericordia de ti" 
+  }; 
+  
+        
+  if(tiempoDeVideo > 319) {
+   a1.innerText = "iev toghutiún shnorhestsé   \n\ny conceda perdón" 
+  }; 
+  
+  if(tiempoDeVideo > 321) {
+    a1.innerText = "  amenain hantsanats kots, \n\na todas tus transgresiones" 
+   }; 
+   
+
+          
+  if(tiempoDeVideo > 324) {
+   a1.innerText = "antseluin iev aiymus \n\ndel pasado y del presente" 
+  }; 
+  
+
+            
+  if(tiempoDeVideo > 326) {
+   a1.innerText = "iev harachigaien prguestsé   \n\ny te libre de futuras tentaciones" 
+  }; 
+  
+  if(tiempoDeVideo > 328.5) {
+    a1.innerText = "hasdadestsé hamenain kordz@s   \n\ny te confirme en todas las" 
+   };
+
+       
+  if(tiempoDeVideo > 331) {
+   a1.innerText = "parís iev hankustsé   \n\nbuenas obras y te dé descanso  " 
+  }; 
+
+  if(tiempoDeVideo > 333) {
+    a1.innerText = "  @zkez y hantertseal guian@s Amén \n\n en paz en la vida eterna. Amén" 
+   } 
+  ;
+
+    
+  if(tiempoDeVideo > 335.5) {
+   a1.innerText = "Azadestsé iev @ztsez martasern   \n\nQue el Dios misericordioso  " 
+  }; 
+   
+
+     
+  if(tiempoDeVideo > 338) {
+   a1.innerText = " Asdvadz iev toghtsé \n\nos libre y perdone de todas vuestras" 
+  }; 
+
+
+  if(tiempoDeVideo > 340) {
+    a1.innerText = "Zamenain hantsans tser,   \n\ntransgresiones que os de tiempo" 
+   }; 
+
+   if(tiempoDeVideo > 343) {
+    a1.innerText = "datsé yamanag abashjareló iev \n\npara el arrepentimiento y para realizar" 
+   };
+
+   if(tiempoDeVideo > 345) {
+    a1.innerText = "kordzeló @zparís,    \n\nbuenas obras, que guíe  " 
+   }; 
+
+   if(tiempoDeVideo > 347) {
+    a1.innerText = " ughestsé iev zarachaga guiáns tser \n\n  vuestra vida futura con la gracia" 
+   }; 
+
+   if(tiempoDeVideo > 349) {
+    a1.innerText = "shnorhok Surp Hokvuin   \n\ndel Espíritu Santo.  " 
+   }; 
+
+   if(tiempoDeVideo > 351) {
+    a1.innerText = " hzor@n iev voghormadz@n \n\nPoderoso y misericordioso, " 
+   }; 
+
+   if(tiempoDeVideo > 354) {
+    a1.innerText = "iev Nmá parrk havidians. Amén\n\ny a El, Gloria eterna. Amén." 
+   }; 
+/**/  
+
+
+   if(tiempoDeVideo > 357) {
+    a1.innerText = "Hishestsír iev @zmez arrachi  \n\nRecuérdanos también en la  " 
+   }; 
+
+   if(tiempoDeVideo > 359) {
+    a1.innerText = "  anmah karrinn Asdudzó\n\npresencia del immortal Cordero de Dios" 
+   };
+
+   if(tiempoDeVideo > 361) {
+    a1.innerText = "Hisheal lichík arrachí  \n\nSerán recordados en la" 
+   }; 
+
+   if(tiempoDeVideo > 363) {
+    a1.innerText = "  anmah karrinn Asdudzó\n\npresencia del immortal Cordero de Dios" 
+   }; 
+   
+   if(tiempoDeVideo > 366.5) {
+    a1.innerText = "Aghaghaguetsék ar  \n\nClamen al Señor,  " 
+   }; 
+
+   if(tiempoDeVideo > 367.5) {
+    a1.innerText = "  Der amenain ierguir \n\ntodos en la tierra, sirvan al" 
+   }; 
+
+   if(tiempoDeVideo > 369) {
+    a1.innerText = "dzarraietsék Dearrn urajuteamp\n\nSeñor con alegría"
+   } 
+
+   /* */
+
+   if(tiempoDeVideo > 371) {
+    a1.innerText = "Mdék arrachí Norá  \n\nEntren en su presencia"
+   } 
+
+   if(tiempoDeVideo > 373) {
+    a1.innerText = " ts@ndzuteamp dzaneruk zi Na e\n\ncon regocijo, conozcan, pues"
+   } 
+
+   if(tiempoDeVideo > 376) {
+    a1.innerText = "Der Asdvadz mer\n\nEs es nuestro Dios"
+   } 
+
+   if(tiempoDeVideo > 378) {
+    a1.innerText = "Na arar @zmez iev voch mek eak.\n\nEs Él quien nos hizo y no nosotros mismos."
+   } 
+
+   if(tiempoDeVideo > 380) {
+    a1.innerText = "mek yoghovurt iev jash@n, arodí Norá\n\nSomos su pueblo u ovejas de Su rebaño"
+   } 
+
+   if(tiempoDeVideo > 383) {
+    a1.innerText = "M@dék @nt trun@s Norá josdovanuteamp\n\nEntren por sus puertas con confesión"
+   } 
+
+   if(tiempoDeVideo > 385.5) {
+    a1.innerText = "iev orhnuteamp y harg@s Norá\n\ny con bendición a su casa"
+   } 
+
+   if(tiempoDeVideo > 388) {
+    a1.innerText = "Josdován iegherúk Dearr@n, \n\nConfiesen al Señor, "
+   } 
+   if(tiempoDeVideo > 390) {
+    a1.innerText = " iev orhnetsék zanún Norá\n\n y bendigan Su nombre"
+   }
+
+   if(tiempoDeVideo > 392) {
+    a1.innerText = "kaghts@r e Der, havidean e voghormutiún Norá  \n\nEs dulce el Señor, es eterna Su misericordia se extiende"
+   } 
+ 
+
+   if(tiempoDeVideo > 395) {
+    a1.innerText = "hazké minchev hazk e dyshmardutiún Norá  \n\nde generación en generación y Su verdad. "
+   } 
+ 
+
 
 
 
